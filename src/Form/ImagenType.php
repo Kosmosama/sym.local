e28fb5774e856cc82b144b3b4c777f77220a7d54
Kosmosama <luismg0408@gmail.com>
2025-01-08 11:34:06 +0100

    Feat: 4.2.

--- src/Form/ImagenType.php
+++ src/Form/ImagenType.php
@@ -5,6 +5,10 @@ namespace App\Form;
 use App\Entity\Imagen;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ImagenType extends AbstractType
@@ -12,13 +16,49 @@ class ImagenType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nombre')
-            ->add('descripcion')
-            ->add('categoria')
-            ->add('numVisualizaciones')
-            ->add('numLikes')
-            ->add('numDownloads')
-        ;
+            ->add('nombre', TextType::class, [
+                'label' => 'Nombre:',
+                'label_attr' => ['class' => 'etiqueta'],
+                'required' => true,
+            ])
+            ->add('descripcion', TextType::class, [
+                'label' => 'Descripción:',
+                'required' => false,
+                'label_attr' => ['class' => 'etiqueta']
+            ])
+            ->add('categoria', NumberType::class, [
+                'label' => 'Categoría:',
+                'label_attr' => ['class' => 'etiqueta', 'value' => '1'],
+                'data' => '1',
+            ])
+            ->add('numVisualizaciones', NumberType::class, [
+                'label' => 'Número de Visualizaciones:',
+                'required' => false,
+                'label_attr' => ['class' => 'etiqueta']
+            ])
+            ->add('numLikes', NumberType::class, [
+                'label' => 'Número de Likes:',
+                'required' => false,
+                'label_attr' => ['class' => 'etiqueta']
+            ])
+            ->add('numDownloads', NumberType::class, [
+                'label' => 'Número de Descargas:',
+                'required' => false,
+                'label_attr' => ['class' => 'etiqueta']
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Las contraseñas deben coincidir.',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Password',
+                    'label_attr' => ['class' => 'etiqueta']
+                ],
+                'second_options' => [
+                    'label' => 'Repetir Password',
+                    'label_attr' => ['class' => 'etiqueta']
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
